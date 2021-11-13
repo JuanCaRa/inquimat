ba375685375ff827d5011ff48be2afd5bb34f561
JuanCaRa <68621886+JuanCaRa@users.noreply.github.com>
2021-11-12 21:57:05 -0500

    Add files via upload

---
+++ inquimat.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+st.write("# Terminos y Condiciones")
+st.write('''
+No es posible garantizar una nota debido a las variables que puede representar y al tipo de profesor que puedas tener, del cual nosotros no tenemos referencias ni conocemos su metodología. Lo que sí podemos garantizar es un correcto desarrollo del trabajo siguiendo los lineamientos que tú nos indiques en la cotización. **Tenemos 2 años de experiencia** y el 95% de nuestros resultados han sido satisfactorios.
+
+### Correcciones:
+- Solo realizamos correcciones sobre las pautas enviadas en la primera cotización. Si se deben agregar puntos o contenido, habrá un cobro adicional por ser trabajo extra.
+- Ten en cuenta que realizamos el trabajo según lo indicas en las pautas anteriormente enviadas por ti en la cotización.  
+- No realizamos correcciones después de **10 días hábiles** de entregado el trabajo.
+
+### Reembolso:
+- Se realiza reembolso en el mismo plazo de las correcciones ( 10 días hábiles) y si en lo que el cliente solicitó hubo algo incorrecto o mal desarrollado y no se puede corregir. **_Si la nota es porque enviaste el archivo sin cambiarle el nombre, Inquimat Asesorías no se hace responsable_**. Si es un concepto agregado que no hayamos hecho nosotros en no se tendrá en cuenta en el reembolso. 
+- Se realiza reembolso si el trabajo no es entregado en la fecha y hora pactada. A menos de que se pueda tener más plazo (puede ocurrir por causas como calamidad, enfermedad del tutor/a, entre otros). 
+- Si la nota obtenida es menor a 3.0 (según la rúbrica de calificación de tu institución) y ya no se puede corregir el trabajo. Para esto se deben presentar comprobantes de que la nota es menor a 3.0, los puntos o desarrollo que estuvieron mal realizados con su respectiva justificación del porqué están mal realizados.         
+
+''')
